aa88e915b563941fd7e058b28995eabc875594ea
amari <junior_maza16@hotmail.com>
2023-10-30 21:13:46 -0300

    Mentoria 15/06/2023 Teste de API - Post do User na PetStore

---
+++ src/test/java/entities/UserEntity.java
@@ -0,0 +1,14 @@
+package entities;
+
+public class UserEntity { // Entidade que representa o usuário no sistema
+
+     public int id; // Identidade
+     public String username; // Nome
+     public String firstName; // Primeiro Nome
+     public String lastName; // Sobrenome
+     public String email;   // E-mail
+     public String password; // Senha
+     public String phone;   // Telefone
+     public int userStatusString; // Status Usuário
+
+}
