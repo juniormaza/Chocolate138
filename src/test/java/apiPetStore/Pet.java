2ead5bbc155c9984900e0a1cc6ece783aefb29cc
amari <junior_maza16@hotmail.com>
2023-11-01 19:44:54 -0300

    Teste de API - Post/Pet na PetStore

--- src/test/java/apiPetStore/Pet.java
+++ src/test/java/apiPetStore/Pet.java
@@ -1,31 +1,66 @@
+// - Pacote
 package apiPetStore;
+// 2 - Bibliotecas
+// 3 - Classe
+
+import com.google.gson.Gson;
+import entities.PetEntity;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 public class Pet {
-    public int id;
-    public Category category;
-    public String name;
-    public String[] photoUrls;
-    public Tag[] tags;
-    public String status;
+    // 3.1 - Atribudos
+    String jsonBody;
+    String ct = "application/json";
+    String uri = "https://petstore.swagger.io/v2/";
 
-public static class Category {
-    public int id;
-    public String name;
+    // 3.1.2 Intanciar Classes Externas
+    Gson gson = new Gson(); // Instancia o objeto de conversão classe para json
 
-    public Category(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+    // Método #EXE - Criar Pet
+    @Test
+    public void testCreatPet(){
+        // Arrange - Configura
+        PetEntity pet = new PetEntity();
+        pet.id = 12345;
+        pet.name = "Ducky";
+        pet.status = "available";
+
+        PetEntity.Category category = new PetEntity.Category(1,"Dogs");
+        pet.category = category;
+
+        PetEntity.Tag tag = new PetEntity.Tag(1,"Frederico");
+        pet.tags = new PetEntity.Tag[]{tag};
+
+        jsonBody = gson.toJson(pet); // converte a entidade no Formato json
+
+
+        // Act - Executa
+
+        // Dado - Quando - Então
+        // Given - When - Then
+        given() // dado
+                .contentType(ct) // tipo do conteudo
+                .log().all()                       // registre tudo na ida
+                .body(jsonBody)                    // corpo da mensagem que será enviada
+        .when()  // quando
+                .post(uri + "pet")
+                // Assert - Valida
+        .then() // então
+                .log().all()        // registre tudo na volta
+                .statusCode(200) //valida a comunicação
+                .body("id", is(12345))
+                .body("name", is("Ducky"))
+                .body("status", is("available"))
+                .body("category.name", is("Dogs"))
+                .body("category.id", is(1))
+                .body("tags[0].name",is("Frederico"))
+                .extract().response(); // extrair a resposta
+        ; // fim da linha do Rest- assured
+
+
+    } // fim do método de criação do pet
 
 }
-
-public static class Tag {
-    public int id;
-    public String name;
-    public Tag(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-}
-}
-
--- src/test/java/entities/PetEntity.java
+++ src/test/java/entities/PetEntity.java
@@ -1,67 +1,31 @@
-// - Pacote
 package entities;
-// 2 - Bibliotecas
-// 3 - Classe
-
-import apiPetStore.Pet;
-import com.google.gson.Gson;
-import entities.UserEntity;
-import org.testng.annotations.Test;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
 
 public class PetEntity {
-    // 3.1 - Atribudos
-    String jsonBody;
-    String ct = "application/json";
-    String uri = "https://petstore.swagger.io/v2/";
+    public int id;
+    public Category category;
+    public String name;
+    public String[] photoUrls;
+    public Tag[] tags;
+    public String status;
 
-    // 3.1.2 Intanciar Classes Externas
-    Gson gson = new Gson(); // Instancia o objeto de conversão classe para json
+public static class Category {
+    public int id;
+    public String name;
 
-    // Método #EXE - Criar Pet
-    @Test
-    public void testCreatPet(){
-        // Arrange - Configura
-        Pet pet = new Pet();
-        pet.id = 12345;
-        pet.name = "Ducky";
-        pet.status = "available";
-
-        Pet.Category category = new Pet.Category(1,"Dogs");
-        pet.category = category;
-
-        Pet.Tag tag = new Pet.Tag(1,"Frederico");
-        pet.tags = new Pet.Tag[]{tag};
-
-        jsonBody = gson.toJson(pet); // converte a entidade no Formato json
-
-
-        // Act - Executa
-
-        // Dado - Quando - Então
-        // Given - When - Then
-        given() // dado
-                .contentType(ct) // tipo do conteudo
-                .log().all()                       // registre tudo na ida
-                .body(jsonBody)                    // corpo da mensagem que será enviada
-        .when()  // quando
-                .post(uri + "pet")
-                // Assert - Valida
-        .then() // então
-                .log().all()        // registre tudo na volta
-                .statusCode(200) //valida a comunicação
-                .body("id", is(12345))
-                .body("name", is("Ducky"))
-                .body("status", is("available"))
-                .body("category.name", is("Dogs"))
-                .body("category.id", is(1))
-                .body("tags[0].name",is("Frederico"))
-                .extract().response(); // extrair a resposta
-        ; // fim da linha do Rest- assured
-
-
-    } // fim do método de criação do pet
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
 }
+
+public static class Tag {
+    public int id;
+    public String name;
+    public Tag(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+}
+
