9167ef5940b5d0f9946968423dacfec6cc7085cc
amari <junior_maza16@hotmail.com>
2023-11-07 19:08:13 -0300

    Completou o script simples Selenium e criamos um arquivo de feature para a mesma funcionalidade

---
+++ src/main/java/webtest/seleniumSimples.java
@@ -0,0 +1,109 @@
+// 1 - Pacote
+package webtest;
+// 2 - Bibliotecas
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+// - Classe
+
+public class seleniumSimples {
+    // 3.1 Atributos
+    WebDriver driver; // objeto do Selenium WebDriver
+
+    // 3.2 Funções e Métodos de Apoio
+    // Não vamos criar
+
+    // 3.3 Antes do Teste
+    @BeforeMethod
+    public void setUp(){
+        // Intalar e configurar o driver do navegador/browser
+        WebDriverManager.chromedriver().setup(); // douwnload e instalação do Chrome Driver
+
+        // Configurar as opções para o driver do navegador (a partir do Selenium 4.8.0)
+        ChromeOptions options = new ChromeOptions(); // objeto de configuração para Chrome Driver
+        options.addArguments("--remote-allow-origins=*"); // permitir qualquer origem remota
+
+
+        // Instanciar o Selenium como driver de um navegador especifico
+        driver = new ChromeDriver(options); // Instancia o Selenium para Chrome Driver com opções
+        // Configura o tempo geral de espera de elementos em ate 5 segundos
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
+        driver.manage().window().maximize(); // mazimiza a janela do navegador
+
+    }
+
+    // 3.4 Depois do Teste
+    @AfterMethod
+    public  void tearDown(){
+        driver.quit(); // destroi o objeto do selenium WebDriver
+
+    }
+
+    // 3.5 Testes em Si
+    @Test
+    public void testarSelectBackdrop(){
+        // Abrir a página inicial do site SauceDemo
+        driver.get("https://www.saucedemo.com");
+
+        // Digitar o usuário e a senha
+        // Clicar no elemento antes de escrever
+        driver.findElements(By.id("user-name"));
+        WebElement username = driver.findElement(By.id("user-name")); // Controla a caixa de texto
+        username.click(); // clica na caixa de texto username
+        username.clear(); // limpa a caixa de texto
+        username.sendKeys("standard_user"); // escreve na caixa (colar o texto)
+        // username.sendKeys(Keys.chord("standard_user")); // escreve na caixa (letra por letra)
+
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+        driver.findElement(By.id("login-button")).click();
+
+        // Trancição de página / Carregamento de nova página (lentidão)
+
+        // Verificar se estamos na página interna (se conseguimos entrar)
+        // Verifica a palavra "Products" em determinado elemento
+        assertEquals(driver.findElement(By.cssSelector("span.title")).getText(),"Products");
+        // Verifica se está presente o elemento do carrinho
+        assertTrue(driver.findElement(By.id("shopping_cart_container")).isDisplayed());
+
+        // Selecionar o produto que seria o id n° 4
+        driver.findElement(By.id("item_4_title_link")).click();
+        // Transição de tela para a página do produto
+
+        // Validar o nome e o valor
+        assertEquals(driver.findElement(By.cssSelector("div.inventory_item_name")).getText(),
+                "Sauce Labs Backpack");
+        assertEquals(driver.findElement(By.cssSelector("div.inventory_item_price")).getText(),
+                "$29.99");
+
+        // Clicar no botão Adionar no Carrinho
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+
+        // Clica no icone do Carrinho
+        driver.findElement(By.id("shopping_cart_container")).click();
+        // Outra transição/carregamento de página
+
+        // Verificar o Titulo da Página, nome do produto, quantidade e preco
+        assertEquals(driver.findElement(By.cssSelector("span.title")).getText(), "Your Cart");
+        assertEquals(driver.findElement(By.cssSelector("item_4_title_link")).getText(), "Sauce Labs Backpack");
+        assertEquals(driver.findElement(By.cssSelector("div.cart_quantity_label")).getText(), "1");
+        assertEquals(driver.findElement(By.cssSelector("div.inventory_item_price")).getText(),"$29.99");
+
+    }
+
+
+}
---
+++ src/test/resources/features/selectProduct.feature
@@ -0,0 +1,26 @@
+# #language: pt
+#  Funcionalidade: Selecionar produto na Loja
+#    Cenario: Selecionar Produto com Sucesso
+#      Dado que acesso a loja SauceDemo
+#      Quando preencho o usuario e senha
+#      E clica em Login
+#      Entao Exibe o titulo da pagina como "Products"
+#      E exibe o link do carrinho de compras
+#      Quando clica no Produto "Sauce Labs Backpack"
+
+  Feature: Select Product in Store
+    Scenario: Selecting Product with Success
+      Given I access the SauceDemo store
+      When I filled username "standard_user" and password "secret_sauce"
+      And I click in Login
+      Then show page title as "Product"
+      And shows cart's link
+      When I click in product "Sauce Labs Backpack"
+      Then I verify the produtc title "Sauce Labs Backpack"
+      And I verify the product price "$ 29.99"
+      When I click in Add to Cart
+      And I click in Cart icon
+      Then hen I verify then page's title "Your Cart"
+      And I verify the produtc title "Sauce Labs Backpack"
+      And I verify the quantity is "1"
+      And I verify the product price "$ 29.99"
